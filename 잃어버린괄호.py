63de438f5ea5edd83d6286872a57057ca7192f86
slbin-park <smpts00@naver.com>
2021-09-09 00:56:23 +0900

    Add boj 1541 solution

--- 잃어버린괄호.py
+++ 잃어버린괄호.py
@@ -5,8 +5,30 @@ a = input()
 plus = 0
 minus = 0
 strnumber = ''
-for i in range(len(a) - 1):
-    if a[i] != '+' or a[i] != '-':
-        strnumber += a[i]서
-
-print(min(cnt1, cnt0))
+ch = 0
+for i in range(len(a) + 1):
+    if i == len(a) - 1:
+        if ch == 0:
+            plus += int(strnumber)
+            break
+        else:
+            minus += int(strnumber)
+            break
+    if a[i] == '+':
+        if ch == 0:
+            plus += int(strnumber)
+            strnumber = ''
+        else:
+            minus += int(strnumber)
+            strnumber = ''
+    elif a[i] == '-':
+        if ch == 0:
+            ch = 1
+            plus += int(strnumber)
+            strnumber = ''
+        else:
+            minus += int(strnumber)
+            strnumber = ''
+    elif a[i] != '+' and a[i] != '-':
+        strnumber += a[i]
+print(plus - minus)
