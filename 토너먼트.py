8868f07f06808a82c5ed3914ec02eadc1423088f
slbin-park <smpts00@naver.com>
2021-09-17 09:41:39 +0900

    Add boj 1057 solution

--- 토너먼트.py
+++ 토너먼트.py
@@ -1,25 +1,20 @@
-n, m, k = map(int, input().split())
-if m > k:
-    temp = m
-    m = k
-    k = temp
-arr = [i for i in range(1, n + 1)]
+k, n, m = map(int, input().split())
 cnt = 0
-check = 1
-while check:
-    for i in range((len(arr) - 1) // 2):
-        if i + 1 > len(arr) - 1:
-            break
-        else:
-            if arr[i] == m:
-                print(arr[i], arr[i + 1])
-                if arr[i + 1] == k:
-                    check = 0
-                    break
-            elif arr[i] == m or arr[i] == k:
-                del arr[i + 1]
-            else:
-                del arr[i]
-    print(arr)
+
+
+def go_to(n):
+    if n == 1:
+        return 1
+    elif n % 2 == 0:
+        return n // 2
+    elif n % 2 == 1:
+        return n // 2 + 1
+
+
+while 1:
     cnt += 1
-print(cnt)
+    if n == m:
+        break
+    n = go_to(n)
+    m = go순_to(m)
+print(cnt - 1)
