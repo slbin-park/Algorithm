488df5750a848f2e459821e64422f14b2bd2d1d2
slbin-park <smpts00@naver.com>
2021-10-06 09:44:47 +0900

    Add boj 1818 solution

---
+++ 책정리.py
@@ -0,0 +1,14 @@
+import sys
+import bisect
+
+input = sys.stdin.readline
+n = int(input())
+arr = list(map(int, input().split()))
+dp = [arr[0]]
+for i in range(n):
+    if arr[i] > dp[-1]:
+        dp.append(arr[i])
+    else:
+        idx = bisect.bisect_left(dp, arr[i])
+        dp[idx] = arr[i]
+print(n - len(dp))
