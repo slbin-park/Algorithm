9b92a8a6bfe895574b4b338b784f11d3d25ff68f
slbin-park <smpts00@naver.com>
2021-10-29 09:57:38 +0900

    Add boj 1946 solution

--- 신입사원.py
+++ 신입사원.py
@@ -1,2 +1,17 @@
 import sys
-from collections import deque
+
+input = sys.stdin.readline
+n = int(input())
+for i in range(n):
+    res = 1
+    m = int(input())
+    arr = [[0, 0] for i in range(m)]
+    for j in range(m):
+        arr[j] = list(map(int, input().split()))
+    arr.sort(key=lambda x: x[0])
+    prev = arr[0][1]
+    for j in range(1, m):
+        if arr[j][1] < prev:
+            prev = arr[j][1]
+            res += 1
+    print(res)
