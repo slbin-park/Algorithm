cad0b06a101f048cff6afb294b78d736370e0eb3
slbin-park <smpts00@naver.com>
2021-11-09 13:15:14 +0900

    Fix py file

--- 문제/1번.py
+++ 문제/1번.py
@@ -8,12 +8,17 @@ minnum = 100
 while 1:
     print("점수를 입력하세요.(입력할 점수가 없다면 n을 입력하시오 : ", end="")
     n = input().strip()
-    if (n == 'n' or n == 'N'):
+    if n == 'n' or n == 'N':
         break
+    elif n.isnumeric():
+        if not 0 <= int(n) <= 100:
+            print("입력이 잘못 되었습니다.")
+        else:
+            res.append(int(n))
+            maxnum = max(int(n), maxnum)
+            minnum = min(int(n), minnum)
     else:
-        res.append(int(n))
-        maxnum = max(int(n), maxnum)
-        minnum = min(int(n), minnum)
+        print("입력이 잘못 되었습니다.")
 print("점수 리스트 : ", end="")
 print(res)
 print("최고 점수 : ", maxnum)
--- 문제/2번.py
+++ 문제/2번.py
@@ -4,12 +4,13 @@ res = []
 for i in range(1, 51):
     for j in range(1, 51):
         b = sqrt(i * i + j * j)
-        if b == int(b) and b < 50:
+        if b == int(b) and b <= 50:
             a = [i, j, int(b)]
             a.sort()
             # print(a)
             if (a[0], a[1], a[2]) not in res:
                 res.append((i, j, int(b)))
 # res = set(res)
+print("세 변의 길이가 50이하의 정수인 직각 삼각혀의 세변의 쌍 ", len(res), "개 입니다.")
 for i in res:
     print(i)
