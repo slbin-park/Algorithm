987a2ad39b30756c89865d55302da35412d6fbff
slbin-park <smpts00@naver.com>
2021-11-11 23:38:08 +0900

    Add py file

--- 숨바꼭질.py
+++ 숨바꼭질.py
@@ -7,11 +7,15 @@ input = sys.stdin.readline
 def bfs(start, end):
     dq.append((start, 0))
     visited = [0 for i in range(200002)]
-    cnt = 0
+    cnt = 100000
+    global rescnt
     while dq:
         cur, curcnt = dq.popleft()
         if cur == end:
             cnt = curcnt
+            rescnt += 1
+            print(dq)
+        if curcnt > cnt:
             break
         if cur + 1 <= 100000:
             if visited[cur + 1] == 0:
@@ -28,7 +32,9 @@ def bfs(start, end):
     return cnt
 
 
+rescnt = 0
 n, m = map(int, input().split())
 dq = deque()
 res = bfs(n, m)
 print(res)
+print(rescnt)
