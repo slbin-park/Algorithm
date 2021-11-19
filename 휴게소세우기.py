29a77eb059e52904884786a32d316e107a3c8c39
slbin-park <smpts00@naver.com>
2021-11-19 18:33:39 +0900

    Add boj 1477 solution

--- 휴게소세우기.py
+++ 휴게소세우기.py
@@ -4,19 +4,19 @@ input = sys.stdin.readline
 n, m, l = map(int, input().split())
 arr = list(map(int, input().split()))
 arr.append(0)
-arr.sort()
 arr.append(l)
-start, end = 0, arr[n]
-res = l
+arr.sort()
+start, end = 1, arr[-1]
 while start <= end:
     mid = (start + end) // 2
     cnt = 0
+    if mid == 0:
+        break
     for i in range(1, len(arr)):
-        # print(mid)
         cnt += (arr[i] - arr[i - 1] - 1) // mid
     if cnt > m:
         start = mid + 1
     else:
         end = mid - 1
-        res = min(res, mid)
+        res = mid
 print(res)
