3a8b5c921affe19ef1e74db5e088fa991b987873
slbin-park <smpts00@naver.com>
2021-11-20 17:39:50 +0900

    Add boj 1300 solution

--- k번째 수.py
+++ k번째 수.py
@@ -1,4 +1,5 @@
 def bi_search(start, end):
+    global res
     mid = 0
     while start <= end:
         mid = (start + end) // 2
@@ -6,14 +7,15 @@ def bi_search(start, end):
         for i in range(1, n + 1):
             temp += min(n, mid // i)
         if temp >= m:
+            res = mid
             end = mid - 1
         else:
             start = mid + 1
-    return mid
 
 
 n = int(input())
 m = int(input())
 arr = []
-res = bi_search(1, n * n)
+res = 0
+bi_search(1, m)
 print(res)
