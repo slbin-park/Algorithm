5590f79df57b9a2dbffcd227d874793c600718fd
slbin-park <smpts00@naver.com>
2021-11-22 22:39:59 +0900

    Add boj 12851 solution

--- 숨바꼭질3.py
+++ 숨바꼭질3.py
@@ -9,17 +9,34 @@ dq = deque()
 dq.append(n)
 dis = [-1 for _ in range(Max)]
 dis[n] = 0
+res = 0
+if n == m:
+    res = 1
 while dq:
     a = dq.popleft()
-    if a == m:
-        break
-    if a * 2 < Max and dis[a * 2] == -1:
-        dq.appendleft(a * 2)
-        dis[a * 2] = dis[a]
-    if a + 1 < Max and dis[a + 1] == -1:
-        dq.append(a + 1)
-        dis[a + 1] = dis[a] + 1
-    if a - 1 >= 0 and dis[a - 1] == -1:
-        dq.append(a - 1)
-        dis[a - 1] = dis[a] + 1
+    if a * 2 < Max:
+        if dis[a * 2] == -1:
+            dq.append(a * 2)
+            dis[a * 2] = dis[a] + 1
+        elif dis[a * 2] == dis[a] + 1:
+            dq.append(a * 2)
+        if a * 2 == m and dis[a] + 1 == dis[m]:
+            res += 1
+    if a + 1 < Max:
+        if dis[a + 1] == -1:
+            dq.append(a + 1)
+            dis[a + 1] = dis[a] + 1
+        elif dis[a + 1] == dis[a] + 1:
+            dq.append(a + 1)
+        if a + 1 == m and dis[a] + 1 == dis[m]:
+            res += 1
+    if a - 1 >= 0:
+        if dis[a - 1] == -1:
+            dq.append(a - 1)
+            dis[a - 1] = dis[a] + 1
+        elif dis[a - 1] == dis[a] + 1:
+            dq.append(a - 1)
+        if a - 1 == m and dis[a] + 1 == dis[m]:
+            res += 1
 print(dis[m])
+print(res)
