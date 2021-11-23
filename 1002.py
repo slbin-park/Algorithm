c618cc0dd80ef3a223406b7936ae3537a9c4e030
slbin-park <smpts00@naver.com>
2021-11-23 22:08:39 +0900

    Change 1002.py

--- 1002.py
+++ 1002.py
@@ -1,14 +1,15 @@
 import math
 
 n = int(input())
-for _ in range(n):# 1부터 5까지
-    x1,y1,r1,x2,y2,r2 = map(int,input().split())
-    distance = math.sqrt((x1-x2)**2 + (y1-y2)**2);
-    if(distance == 0 and r1 == r2 ) :
+for _ in range(n):  # 1부터 5까지
+    x1, y1, r1, x2, y2, r2 = map(int, input().split())
+    distance = math.sqrt((x1 - x2)**2 + (y1 - y2)**2)
+    if (distance == 0 and r1 == r2):
         print(-1)
-    elif(distance == abs(r1-r2) or distance ==r1+r2):
+    elif (distance == abs(r1 - r2) or distance == r1 + r2):
         print(1)
-    elif(abs(r1-r2) < distance <r1+r2) : 
+    elif (abs(r1 - r2) < distance < r1 + r2):
         print(2)
-    else :
+    else:
         print(0)
+# 송승준병신
