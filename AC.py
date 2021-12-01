7529c8a9963580b6883ae7675a26bd373844ce89
slbin-park <smpts00@naver.com>
2021-12-01 11:34:08 +0900

    Add boj 5430 solution

--- AC.py
+++ AC.py
@@ -8,33 +8,29 @@ for i in range(n):
     m = int(input())
     flag = 1
     arr = input().strip()
-    arr = arr.replace("[", "")
-    arr = arr.replace("]", "")
-    arr = arr.split(',')
-    dq = deque(arr)
-    if len(arr) == 1:
+    dq = deque(arr[1:-1].split(','))
+    if m == 0:
         dq = deque()
-    arr = []
+    R = 0
     for i in range(len(a)):
         if a[i] == 'R':
-            for i in range(len(dq)):
-                arr.append(dq.pop())
-            dq = deque(arr)
-            arr = []
+            R += 1
         elif a[i] == 'D':
             if len(dq) == 0:
                 print('error')
                 flag = 0
                 break
             else:
-                dq.popleft()
+                if R % 2 == 0:
+                    dq.popleft()
+                else:
+                    dq.pop()
+
     if flag == 0:
         continue
     else:
-        print('[', end='')
-        for i in dq:
-            if i == dq[-1]:
-                print(i, end='')
-            else:
-                print(i, end=',')
-        print(']')
+        if R % 2 == 0:
+            print('[' + ",".join(dq) + ']')
+        else:
+            dq.reverse()
+            print('[' + ",".join(dq) + ']')
