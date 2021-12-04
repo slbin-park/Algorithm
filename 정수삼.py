909db82964fdd79821eb1ed7339a783f3e6860b0
slbin-park <smpts00@naver.com>
2021-12-04 22:59:53 +0900

    Add boj solution prg

---
+++ 정수삼.py
@@ -0,0 +1,25 @@
+triangle = [[7], [3, 8], [8, 1, 0], [2, 7, 4, 4], [4, 5, 2, 6, 5]]
+res = []
+for i in range(len(triangle)):
+    res.append(triangle[i])
+print(res)
+
+
+def solution(triangle):
+    arr = [[] for i in range(len(triangle))]
+    for i in range(len(triangle)):
+        for j in range(i + 1):
+            arr[i].append(0)
+    arr[0][0] = triangle[0][0]
+    for i in range(1, len(triangle)):
+        arr[i][0] = arr[i - 1][0] + triangle[i][0]
+        arr[i][i] = arr[i - 1][i - 1] + triangle[i][i - 1]
+        for j in range(1, i):
+            arr[i][j] = max(arr[i - 1][j - 1], arr[i - 1][j]) + triangle[i][j]
+    print(arr)
+
+    answer = 0
+    return answer
+
+
+print(solution(triangle))
