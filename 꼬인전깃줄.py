dc6d10e041de929cb3de6769aea40503f37fad81
slbin-park <smpts00@naver.com>
2022-01-04 14:48:45 +0900

    Add boj 1365 solution

--- 꼬인전깃줄.py
+++ 꼬인전깃줄.py
@@ -1,15 +1,15 @@
 import bisect
+import sys
 
-x = int(input())
+input = sys.stdin.readline
+n = int(input())
 arr = list(map(int, input().split()))
-
 dp = [arr[0]]
-
-for i in range(x):
+for i in range(n):
     if arr[i] > dp[-1]:
         # 오른쪽에서 왼쪽방향 -1
         dp.append(arr[i])
     else:
         idx = bisect.bisect_left(dp, arr[i])
         dp[idx] = arr[i]
-print(len(dp))
+print(n - len(dp))
