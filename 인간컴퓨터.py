6f2bc8bbb80e02d08ef02b72ce2cd45f530cd757
slbin-park <smpts00@naver.com>
2022-01-06 14:53:27 +0900

    Add boj 16139 solution

--- 인간컴퓨터.py
+++ 인간컴퓨터.py
@@ -4,10 +4,19 @@ input = sys.stdin.readline
 
 name = input().strip()
 n = int(input())
+arr = [[0 for i in range(26)] for i in range(len(name))]
+arr[0][ord(name[0]) - 97] = 1
+for i in range(1, len(name)):
+    arr[i][ord(name[i]) - 97] = 1
+    for j in range(26):
+        arr[i][j] += arr[i - 1][j]
 for i in range(n):
     a = input().split()
-    sum = 0
-    for j in range(int(a[1]), int(a[2]) + 1):
-        if name[j] == a[0]:
-            sum += 1
-    print(sum)
+    if int(a[1]) > 0:
+        res = arr[int(
+            a[2])][ord(a[0]) - 97] - arr[int(a[1]) - 1][ord(a[0]) - 97]
+    else:
+        res = arr[int(a[2])][ord(a[0]) - 97]
+    print(res)
+
+#26개
