7f2edf04173e922eb8a0f3c915172ac57a1b45d0
slbin-park <smpts00@naver.com>
2022-01-10 16:39:00 +0900

    Add boj 2638 solution

--- 치즈.py
+++ 치즈.py
@@ -2,6 +2,22 @@ import sys
 from collections import deque
 
 
+def findbfs():
+    visited[0][0][0] = 0
+    fdq = deque()
+    fdq.append((0, 0))
+    dq.append((0, 0))
+    while fdq:
+        x, y = fdq.popleft()
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if 0 <= nx < n and 0 <= ny < m:
+                if arr[nx][ny] == 0 and visited[nx][ny][0] == 2:
+                    dq.append((nx, ny))
+                    visited[nx][ny][0] = 0
+
+
 def bfs():
     res = 0
     while dq:
@@ -17,6 +33,10 @@ def bfs():
                         dq.append((nx, ny))
                         visited[nx][ny][1] = visited[x][y][1] + 1
                         res = max(res, visited[nx][ny][1])
+                elif arr[nx][ny] == 0 and visited[nx][ny][0] == 2:
+                    visited[nx][ny][0] = 0
+                    visited[nx][ny][1] = visited[x][y][1]
+                    dq.appendleft((nx, ny))
     return res
 
 
@@ -30,10 +50,7 @@ visited = [[[2, 0] for i in range(m)] for i in range(n)]
 dq = deque()
 for i in range(n):
     arr[i] = list(map(int, input().split()))
-    for j in range(m):
-        if arr[i][j] == 0:
-            dq.append((i, j))
+
+findbfs()
 ans = bfs()
 print(ans)
-for i in range(n):
-    print(visited[i])
