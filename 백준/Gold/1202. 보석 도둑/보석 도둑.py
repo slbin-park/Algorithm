9668b1b59d0f890d2532632ce76f32ae243fbb0f
slbin-park <74503437+slbin-park@users.noreply.github.com>
2022-04-17 23:33:13 +0900

    [Gold II] Title: 보석 도둑, Time: 2208 ms, Memory: 191476 KB -BaekjoonHub

--- 백준/Gold/1202. 보석 도둑/보석 도둑.py
+++ 백준/Gold/1202. 보석 도둑/보석 도둑.py
@@ -1,33 +1,26 @@
-import sys
 import heapq
+import sys
+
 input = sys.stdin.readline
 
-N, K = map(int, input().split())
+n, k = map(int, input().split())
+heap = []
+bag = []
+res = 0
+for i in range(n):
+    m, v = map(int, input().split())
+    heapq.heappush(heap, [m, v])
+for i in range(k):
+    bag.append(int(input()))
+bag.sort()
+heap2 = []
+for i in range(k):
+    while heap and bag[i] >= heap[0][0]:
+        m, v = heapq.heappop(heap)
+        heapq.heappush(heap2, -v)
+    if len(heap2) > 0:
+        res -= heapq.heappop(heap2)
+    elif len(heap) == 0:
+        break
 
-gem = []
-weights = []
-
-for _ in range(N):
-  M,V = map(int, input().split())
-  heapq.heappush(gem, [M,V])
-
-for _ in range(K):
-  heapq.heappush(weights, int(input()))
-
-sum = 0
-
-capable_gem = []
-
-for _ in range(K):
-  capacity = heapq.heappop(weights)
-  
-  while gem and capacity >= gem[0][0]:
-    [weight, price] = heapq.heappop(gem)
-    heapq.heappush(capable_gem, -price)
-  
-  if capable_gem:
-    sum -= heapq.heappop(capable_gem)
-  elif not gem:
-    break
-
-print(sum)
+print(res)
