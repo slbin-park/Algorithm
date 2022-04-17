dd665e600950807251ee95ccf6f10f325012da32
slbin-park <74503437+slbin-park@users.noreply.github.com>
2022-04-18 00:39:29 +0900

    [Gold V] Title: 최소 회의실 개수, Time: 488 ms, Memory: 128628 KB -BaekjoonHub

---
+++ 백준/Gold/19598. 최소 회의실 개수/최소 회의실 개수.py
@@ -0,0 +1,22 @@
+import heapq
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+arr = []
+for i in range(n):
+    start, end = map(int, input().split())
+    arr.append([start, end])
+arr.sort()
+heap = []
+for start, end in arr:
+    if len(heap) == 0:
+        heapq.heappush(heap, end)
+    else:
+        if heap[0] <= start:
+            heapq.heappop(heap)
+            heapq.heappush(heap, end)
+        else:
+            heapq.heappush(heap, end)
+print(len(heap))
