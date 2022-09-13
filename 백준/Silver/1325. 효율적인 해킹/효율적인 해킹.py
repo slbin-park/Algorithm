b239402e347dd968b2ca05b5bdafe0644fafa227
slbin_park <74503437+slbin-park@users.noreply.github.com>
2022-09-13 15:41:12 +0900

    [Silver I] Title: 효율적인 해킹, Time: 11928 ms, Memory: 215508 KB -BaekjoonHub

---
+++ 백준/Silver/1325. 효율적인 해킹/효율적인 해킹.py
@@ -0,0 +1,36 @@
+import sys
+from collections import deque
+
+input = sys.stdin.readline
+N, M = map(int, input().split())
+graph = [[] for _ in range(N + 1)]
+cnt = [0] * (N + 1)
+
+for _ in range(M):
+    a, b = map(int, input().split())
+    graph[b].append(a)
+
+
+def bfs(start):
+    visited = [0] * (N + 1)
+    CNT = 0
+    q = deque()
+    visited[start] = 1
+    q.append(start)
+    while q:
+        CNT += 1
+        node = q.popleft()
+        for nd in graph[node]:
+            if visited[nd] == 0:
+                visited[nd] = 1
+                q.append(nd)
+    return CNT
+
+
+for i in range(1, N + 1):
+    cnt[i] = bfs(i)
+
+c = max(cnt)
+for j in range(1, N + 1):
+    if cnt[j] == c:
+        print(j, end=' ')
