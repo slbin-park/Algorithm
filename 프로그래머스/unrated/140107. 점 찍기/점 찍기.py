cafbf7d830c9fb6c6f255a11f1b43a96f729ef97
slbin_park <74503437+slbin-park@users.noreply.github.com>
2022-12-05 03:42:02 +0900

    [unrated] Title: 점 찍기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/140107. 점 찍기/점 찍기.py
+++ 프로그래머스/unrated/140107. 점 찍기/점 찍기.py
@@ -1,7 +1,3 @@
 import math
 def solution(k, d):
-    answer = 0
-    for i in range(0,d+1,k):
-        a = math.sqrt(d**2 - i**2)
-        answer += a//k +1
-    return answer
+    return sum([math.sqrt(d**2-i**2)//k+1 for i in range(0,d+1,k)])
