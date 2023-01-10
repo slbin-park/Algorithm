4e09ff64d4ac52adfec8285b3f310b7fb006aa9c
slbin_park <74503437+slbin-park@users.noreply.github.com>
2023-01-10 22:01:21 +0900

    [Silver I] Title: 배열 돌리기 1, Time: 624 ms, Memory: 245852 KB -BaekjoonHub

--- 백준/Silver/16926. 배열 돌리기 1/배열 돌리기 1.py
+++ 백준/Silver/16926. 배열 돌리기 1/배열 돌리기 1.py
@@ -7,19 +7,27 @@ sys.setrecursionlimit(10**5)
 
 
 def rotate(diff):
-    # 가로축
+
     left_up = arr[diff][diff]
     right_down = arr[n - 1 - diff][m - 1 - diff]
     for i in range(0, m - diff * 2 - 1, 1):
         # 위쪽
-        arr[diff][diff + i] = arr[diff][diff + i + 1]
+        up_dir = diff + i
+        arr[diff][up_dir] = arr[diff][up_dir + 1]
         # 아래쪽
-        arr[n - diff - 1][m - 1 - diff - i] = arr[n - diff - 1][m - 1 - diff -
-                                                                1 - i]
+        down_dir = diff + i
+        down_x = n - diff - 1
+        down_y = m - 1
+        arr[down_x][down_y - down_dir] = arr[down_x][down_y - down_dir - 1]
 
     for i in range(1, n - diff * 2 - 1, 1):
-        arr[n - i - diff][diff] = arr[n - i - 1 - diff][diff]
-        arr[diff + i - 1][m - diff - 1] = arr[diff + i][m - diff - 1]
+        # 왼쪽
+        left_dir = diff + i
+        arr[n - left_dir][diff] = arr[n - left_dir - 1][diff]
+        # 오른쪽
+        right_dir = diff + i
+        right_x = m - diff - 1
+        arr[right_dir - 1][right_x] = arr[right_dir][right_x]
     arr[diff + 1][diff] = left_up
     arr[n - 1 - diff - 1][m - 1 - diff] = right_down
 
@@ -36,6 +44,7 @@ for i in range(n):
     arr[i] = list(map(int, input().split()))
 
 for i in range(min(n, m) // 2):
+    cnt = (m - (i * 2)) + (n - (i * 2)) - 4
     for k in range(r):
         rotate(i)
 
