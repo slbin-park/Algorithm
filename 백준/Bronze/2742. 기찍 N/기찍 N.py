21447c06c375443d0f85ff158df2ebe5875cea9f
slbin_park <74503437+slbin-park@users.noreply.github.com>
2023-06-29 23:20:18 +0900

    [Bronze IV] Title: 기찍 N, Time: 332 ms, Memory: 132864 KB -BaekjoonHub

---
+++ 백준/Bronze/2742. 기찍 N/기찍 N.py
@@ -0,0 +1,10 @@
+from copy import deepcopy
+import heapq
+from collections import deque
+from itertools import combinations
+import decimal
+import sys
+
+n = int(input())
+for i in range(n, 0, -1):
+    print(i)
