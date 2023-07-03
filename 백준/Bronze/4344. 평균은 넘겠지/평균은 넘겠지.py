69d60f4257f28690b565359bcd8b253d02f9a4ed
slbin_park <74503437+slbin-park@users.noreply.github.com>
2023-07-03 18:20:14 +0900

    [Bronze I] Title: 평균은 넘겠지, Time: 48 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/4344. 평균은 넘겠지/평균은 넘겠지.py
+++ 백준/Bronze/4344. 평균은 넘겠지/평균은 넘겠지.py
@@ -1,22 +1,13 @@
-from copy import deepcopy
-import heapq
-from collections import deque
-from itertools import combinations
-import decimal
-import sys
+num = int(input())
 
-context = decimal.getcontext()
-context.rounding = decimal.ROUND_HALF_UP
-sys.setrecursionlimit(10**5)
-
-n = int(input())
-for i in range(n):
-    arr = list(map(int, input().split()))
-    arrSum = sum(arr) - arr[0]
-    avg = arrSum / arr[0]
+for _ in range(num):
+    scores = list(map(int, input().split()))
+    avg = sum(scores[1:])/scores[0]
+    
     cnt = 0
-    for j in range(1, len(arr)):
-        if arr[j] > avg:
+    for i in scores[1:]:
+        if i > avg:
             cnt += 1
-    per = round(decimal.Decimal(cnt * 100 / arr[0]), 3)
-    print("%.3f" % per + "%")
+            
+    per = (cnt/scores[0])*100
+    print('%.3f' %per + '%')
