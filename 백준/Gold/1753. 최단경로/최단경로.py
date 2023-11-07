b63681208a47d090857db0792402b453ffba86a3
slbin_park <74503437+slbin-park@users.noreply.github.com>
2023-11-07 11:55:20 +0900

    [Gold IV] Title: 최단경로, Time: 644 ms, Memory: 153712 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.py
+++ 백준/Gold/1753. 최단경로/최단경로.py
@@ -1,38 +1,30 @@
-from copy import deepcopy
 import heapq
 import sys
-from collections import deque
-from itertools import combinations
-from xml.dom.minicompat import NodeList
-sys.setrecursionlimit(10**5)
-# list(combinations(items, 2))
-INF = 1e9
+
 input = sys.stdin.readline
+INF = 1e9
+n, m = map(int, input().split())
 
-def dijkstra():
-    heap = []
-    heapq.heappush(heap,[0,start])
-    dis[start] = 0
-    while heap:
-        cost,idx = heapq.heappop(heap)
-        if dis[idx] < cost : 
-            continue
-        for next,cst in arr[idx]:
-            if dis[next] > cost+cst:
-                dis[next] = cost+cst
-                heapq.heappush(heap,[cost+cst,next])
-    
+arr = [[] for i in range(n + 1)]
+heap = []
+dis = [1e9 for i in range(n + 1)]
 
-n, m = map(int,input().split())
-arr = [[]for i in range(n+1)]
-dis = [INF] * (n+1)
 start = int(input())
+dis[start] = 0
 for i in range(m):
-    u,v,w = map(int,input().split())
-    arr[u].append([v,w])
-dijkstra()
-for i in range(1,n+1):
-    if dis[i] == INF:
-        print('INF')
+    u, v, w = map(int, input().split())
+    arr[u].append([v, w])
+heapq.heappush(heap, [0, start])
+while heap:
+    cost, idx = heapq.heappop(heap)
+    if dis[idx] < cost:
         continue
-    print(dis[i])
+    for next, cst in arr[idx]:
+        if dis[next] > cost + cst:
+            dis[next] = cost + cst
+            heapq.heappush(heap, [cost + cst, next])
+for i in range(1, n + 1):
+    if dis[i] == INF:
+        print("INF")
+    else:
+        print(dis[i])
