055324d9a4a1cb8a4051a1f158bfc9f5d971e1df
slbin_park <74503437+slbin-park@users.noreply.github.com>
2023-11-22 09:32:58 +0900

    [level 3] Title: 코딩 테스트 공부, Time: 5.31 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/3/118668. 코딩 테스트 공부/코딩 테스트 공부.py
+++ 프로그래머스/3/118668. 코딩 테스트 공부/코딩 테스트 공부.py
@@ -1,23 +1,21 @@
 def solution(alp, cop, problems):
     answer = 1e9
-    # 목표치
     maxAlp = 0
     maxCop = 0
-    problems.append([0,0,0,1,1])
-    problems.append([0,0,1,0,1])
     for i in range(len(problems)):
         maxAlp = max(maxAlp,problems[i][0])
         maxCop = max(maxCop,problems[i][1])
     dp = [[1e9 for i in range(maxCop+1)]for i in range(maxAlp+1)]
-    alp = min(alp,maxAlp)
-    cop = min(cop,maxCop)
-    dp[alp][cop] = 0
-    for i in range(alp,maxAlp+1):
-        # j = 코딩력
-        for j in range(cop,maxCop+1):
+    dp[min(alp,maxAlp)][min(cop,maxCop)] = 0
+    for i in range(min(alp,maxAlp),maxAlp+1):
+        for j in range(min(cop,maxCop),maxCop+1):
+            mi = min(i+1,maxAlp)
+            mj = min(j+1,maxCop)
+            dp[mi][j] = min(dp[mi][j],dp[i][j]+1)
+            dp[i][mj] = min(dp[i][mj],dp[i][j]+1)
             for alp_req,cop_req,alp_rwd,cop_rwd,cost in problems:
-                if i >= alp_req and j >= cop_req:
-                    mAlp = min(maxAlp,i+alp_rwd)
-                    mCop = min(maxCop,j+cop_rwd)
-                    dp[mAlp][mCop] = min(dp[i][j] + cost, dp[mAlp][mCop])
+                if i>=alp_req and j>=cop_req:
+                    mi = min(maxAlp,i+alp_rwd)
+                    mj = min(maxCop,j+cop_rwd)
+                    dp[mi][mj] = min(dp[mi][mj],dp[i][j]+cost)
     return dp[-1][-1]
